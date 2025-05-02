3e39f9b988e1127631776c382dfcf17d58a13737
ken-jun <kensato1005@icloud.com>
2025-05-03 04:42:04 +0900

    form画面の修正

--- demo/src/main/java/com/example1/demo/CashForm/CashForm.java
+++ demo/src/main/java/com/example1/demo/CashForm/CashForm.java
@@ -1,10 +1,21 @@
 package com.example1.demo.CashForm;
 
+
+
+import java.sql.Date;
+
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class CashForm {
-    private int category;
-    private int income;
-    private String date;
+    private Integer id;
+
+    @NotBlank(message="入力してください入力してください")
+    private String income;
+
+    @NotBlank(message="入力してください")
+    private String category;
+
+    private Date date;
 }
--- demo/src/main/java/com/example1/demo/Controller/CashController.java
+++ demo/src/main/java/com/example1/demo/Controller/CashController.java
@@ -4,13 +4,17 @@ package com.example1.demo.Controller;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example1.demo.CashForm.CashForm;
 import com.example1.demo.Repository.CashRepository;
 import com.example1.demo.entity.Cash;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
 
@@ -31,7 +35,21 @@ public class CashController {
     @GetMapping("/cash/new")
     public ModelAndView showNewCashForm(ModelAndView mv) {
         mv.setViewName("cashForm");
-        mv.addObject("cash", new Cash());
+        mv.addObject("cashData", new CashForm());
         return mv;
     }
+
+   @PostMapping("/cash/new")
+public String addNewCash(@ModelAttribute @Valid CashForm cashData, BindingResult result) {
+    if (result.hasErrors()) {
+        return "cashForm"; 
+    }
+    Cash cash = new Cash();
+    cash.setIncome(cashData.getIncome());
+    cash.setCategory(cashData.getCategory());
+    cash.setDate(cashData.getDate());
+    cashRepository.save(cash);
+    return "redirect:/";
+}
+
 }
