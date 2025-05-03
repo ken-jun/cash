c50a37245dff44244cec4adc761e1299d333c8d6
ken-jun <kensato1005@icloud.com>
2025-05-03 18:43:12 +0900

    データ保存機能の追加

--- demo/src/main/java/com/example1/demo/CashForm/CashForm.java
+++ demo/src/main/java/com/example1/demo/CashForm/CashForm.java
@@ -4,18 +4,29 @@ package com.example1.demo.CashForm;
 
 import java.sql.Date;
 
+import com.example1.demo.entity.Cash;
+
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CashForm {
     private Integer id;
 
-    @NotBlank(message="入力してください入力してください")
-    private String income;
+    @NotNull
+    private Integer income;
 
     @NotBlank(message="入力してください")
     private String category;
 
     private Date date;
+
+    public Cash toEntity() {
+        Cash cash = new Cash();
+        cash.setIncome(income);
+        cash.setCategory(category);
+        cash.setDate(date);
+        return cash;
+    }
 }
